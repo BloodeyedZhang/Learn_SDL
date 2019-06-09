6e118023e8140e9f76739e7b08e7354ae728155f
BloodeyedZhang <512867879@qq.com>
2019-06-09 20:56:59 +0800

    first commit

---
+++ Learn_SDL/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Learn_SDL
+//
+//  Created by 张佳俊 on 2019/6/9.
+//  Copyright © 2019 张佳俊. All rights reserved.
+//
+
+#include <iostream>
+#include <SDL2/SDL.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
